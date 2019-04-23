8e8c684489641649e37b8876dd5ea130b015b44a
Vinicius Kremer Santos <vinicius@ootz.com.br>
2019-04-23 14:37:39 -0300

    Mudando host do server socket

--- assets/js.js
+++ assets/js.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-    var socket = io.connect("http://localhost:3000")
+    // var socket = io.connect("http://localhost:3000")
+    var socket = io.connect("http://socketserver.ootz.com.br")
     var ready = false
 
     $("#submit").submit(function(e){
